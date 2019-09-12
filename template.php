4b97cd44e94ad5d3640ebad28ea93537eec967c6
PackWeb <info@packweb.eu>
2019-09-12 16:15:36 +0300

    Removed code that output hidden site name, etc. (bad for SEO).

--- template.php
+++ template.php
@@ -4,22 +4,6 @@
  */
 
 /**
- * Implements hook_preprocess_header().
- *
- * Always output the site name and slogan, but hide them if they're toggled off
- * (the actual hiding happens in header.tpl.php).
- */
-function pw_theme_preprocess_header(&$variables) {
-  // Get booleans for the name/slogan toggle status.
-  $variables['hide_site_name'] = empty($variables['site_name']);
-  $variables['hide_site_slogan'] = empty($variables['site_slogan']);
-
-  // Populate the name/slogan variables again for output regardless.
-  $variables['site_name'] = filter_xss(config_get('system.core', 'site_name'));
-  $variables['site_slogan'] = filter_xss(config_get('system.core', 'site_slogan'));
-}
-
-/**
  * Implements hook_preprocess_page().
  */
 function pw_theme_preprocess_page(&$variables) {
